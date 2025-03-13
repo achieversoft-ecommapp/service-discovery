c167e98c184e9dc761c1e22ba7eebb04d5e70247
prsgithub <ranjansahoo.prakash@gmail.com>
2025-03-14 00:00:21 +0530

    1st commit

---
+++ src/test/java/com/achieversoft/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.achieversoft.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
